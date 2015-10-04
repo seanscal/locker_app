6f6440ce2efbb12cd9db767a6784fca33461ba42
Eliot Johnson <jpagezoso@gmail.com>
2015-10-04 14:29:26 -0400

    added simple splash screen

--- capstone/AppDelegate.swift
+++ capstone/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // show splash screen for 2 seconds
+        sleep(2)
+        
         return true
     }
 
