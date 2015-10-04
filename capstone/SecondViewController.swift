9c9d9971460c9801e44d41deef13607b4b84f6b5
Eliot Johnson <eliot.johnson@runkeeper.com>
2015-10-04 12:11:14 -0400

    Initial Commit

---
+++ capstone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  capstone
+//
+//  Created by Eliot Johnson on 10/4/15.
+//  Copyright © 2015 Northeastern. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
