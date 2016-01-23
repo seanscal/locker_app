c686c0757621bcf04977789cdf5919c2e186bc61
Ali Hyder <alihyder23@gmail.com>
2016-01-22 21:51:03 -0500

    New Wirefram Model

---
+++ locker_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  locker_app
+//
+//  Created by Ali Hyder on 1/22/16.
+//  Copyright © 2016 Ali Hyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
