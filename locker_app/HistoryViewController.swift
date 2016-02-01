56c4e5ec1ad7d763fe6223a778a788f77a88a9f9
Eliot Johnson <jpagezoso@gmail.com>
2016-02-01 09:01:56 -0500

    initial work on history screen - hardcoded some sample data for now

--- locker_app/HistoryViewController.swift
+++ locker_app/HistoryViewController.swift
@@ -8,22 +8,24 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController {
-    @IBOutlet weak var mapButton: UIButton!
+class HistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         self.navigationItem.hidesBackButton = true
         
         let backButton = UIBarButtonItem(title: "Map", style: UIBarButtonItemStyle.Plain, target: self, action: "pop")
         self.navigationItem.rightBarButtonItem = backButton
         
         self.navigationItem.title = "History"
-    
     }
-    
+
     func pop() {
         let transition = CATransition()
         transition.duration = kDefaultSegueDuration
@@ -38,6 +40,65 @@ class HistoryViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2    // active and past
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch(section) {
+        case 0:
+            return 1
+        default:
+            return 3
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        //return "Section \(section)"
+        
+        switch(section) {
+        case 0:
+            return "Active"
+        default:
+            return "Past"
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        switch(indexPath.section) {
+        case 0:
+            cell.textLabel?.text = "NEU Hub - 1 hr, 15 min"
+            cell.detailTextLabel?.text = "$4.99"
+        default:
+            switch(indexPath.row) {
+            case 0:
+                cell.textLabel?.text = "NYC Hub - 0 hr, 45 min"
+                cell.detailTextLabel?.text = "$2.01"
+            case 1:
+                cell.textLabel?.text = "NEU Hub - 0 hr, 32 min"
+                cell.detailTextLabel?.text = "$2.23"
+            default:
+                cell.textLabel?.text = "Prudential Hub - 2 hr, 36 min"
+                cell.detailTextLabel?.text = "$10.54"
+            }
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat.init(72)
+    }
+    
     
     
 }
