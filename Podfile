f238190ab51636c196268e1d2334894e50be6a22
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-02-10 16:43:02 -0500

    need to adjust button

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ use_frameworks!
 pod 'GoogleMaps'
 pod 'Alamofire', '~> 3.0'
 pod 'ObjectMapper', '~> 1.1'
+pod 'Google/SignIn'
 target ‘locker_app’ do
     pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
 end
---
+++ locker_app/locker_app-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  locker_app-Bridging-Header.h
+//  locker_app
+//
+//  Created by Sean on 2/6/16.
+//  Copyright © 2016 Ali Hyder. All rights reserved.
+//
+
+#import <GoogleSignIn/GoogleSignIn.h>
+#import <Google/SignIn.h>
+
