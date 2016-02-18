1bee80fb99822d70fdfdecb405b33fe8d8f34017
Eliot Johnson <jpagezoso@gmail.com>
2016-02-18 13:50:40 -0500

    added UserSettings class

---
+++ locker_app/UserSettings.swift
@@ -0,0 +1,13 @@
+//
+//  UserSettings.swift
+//  locker_app
+//
+//  Created by Eliot Johnson on 2/18/16.
+//  Copyright © 2016 Ali Hyder. All rights reserved.
+//
+
+import Foundation
+
+class UserSettings {
+    static let userId = 1 // TODO: implement this class
+}
